22682cb239d8dee424b56e4d7a86197e58650eb8
GhostyTongue <50715350+Ghosty-Tongue@users.noreply.github.com>
2024-03-08 10:59:16 -0800

    Update assets_extractor.py

--- assets_extractor.py
+++ assets_extractor.py
@@ -3,57 +3,23 @@ import re
 import shutil
 import hashlib
 import requests
+from tqdm import tqdm
 
 home_directory = os.path.expanduser("~")
-roblox_cache_path = os.path.join(home_directory, 'AppData', 'Local', 'Temp', 'Roblox', 'http')
-roblox_sounds_cache_path = os.path.join(home_directory, 'AppData', 'Local', 'Temp', 'Roblox', 'sounds')
-extracted_assets_output_path = os.path.join(home_directory, 'YOUR_OUTPUT_PATH_FOR_EXTRACTED_ASSETS')
+roblox_sounds_cache_path = os.path.join(home_directory, 'Library', 'Caches', 'com.roblox.Roblox', 'sounds')
+extracted_assets_output_path = os.path.join(home_directory, 'Downloads', 'extracted_assets')
 
-already_extracted_assets = []
-
+# Create the output directory if it doesn't exist
 if not os.path.exists(extracted_assets_output_path):
     print(f"The following path: {extracted_assets_output_path} doesn't exist! Creating it...")
     os.makedirs(extracted_assets_output_path)
 
-print(f"Initializing... Searching through assets cache path: {roblox_cache_path}")
+already_extracted_assets = []
 
-for filename in os.listdir(roblox_cache_path):
-    file_path = os.path.join(roblox_cache_path, filename)
+print(f"Initializing... Searching through sounds cache path: {roblox_sounds_cache_path}")
 
-    if os.path.isfile(file_path):
-        with open(file_path, "r", encoding="latin-1", errors="ignore") as file:
-            file_content = file.read()
-
-        match = re.search(r"https://[A-Za-z0-9]+\.rbxcdn\.com/([0-9]+([A-Za-z]+[0-9]+)+)", file_content)
-        
-        if match:
-            link = match.group(0)
-
-            try:
-                response = requests.get(link)
-                linked_file_content = response.content.decode("latin-1")
-
-                content_checksum = hashlib.md5(response.content).hexdigest()
-
-                if not content_checksum in already_extracted_assets:
-                    if "<roblox!" in linked_file_content:
-                        file_extension = ".rbxm"
-                        print("Extracted RBXM file.")
-                    elif "IHDR" in linked_file_content:
-                        file_extension = ".png"
-                        print("Extracted PNG file.")
-                    else:
-                        continue
-
-                    with open(os.path.join(extracted_assets_output_path, f"{filename}{file_extension}"), "wb") as save_file:
-                        already_extracted_assets.append(content_checksum)
-                        save_file.write(response.content)
-            except requests.exceptions.RequestException as e:
-                print(f"Error fetching asset: {link}. Error message: {str(e)}")
-
-print(f"Done. Searching through sounds cache path: {roblox_sounds_cache_path}")
-
-for filename in os.listdir(roblox_sounds_cache_path):
+# Extracting audio files
+for filename in tqdm(os.listdir(roblox_sounds_cache_path), desc="Extracting audio files"):
     file_path = os.path.join(roblox_sounds_cache_path, filename)
 
     if os.path.isfile(file_path):
@@ -71,4 +37,4 @@ for filename in os.listdir(roblox_sounds_cache_path):
             
             print("Extracted WAV file.")
 
-print("Done. Finished extracting all assets.")
+print("Done. Finished extracting audio files.")
